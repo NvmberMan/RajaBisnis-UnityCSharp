14afc8d0fc4758af1452fc9a6b482b3935a267af
NumberMan <numberman3250@gmail.com>
2023-08-31 13:26:25 +0700

    bug

--- Raja Bisnis/Assets/Script/WorldLight.cs
+++ Raja Bisnis/Assets/Script/WorldLight.cs
@@ -9,9 +9,24 @@ public class WorldLight : MonoBehaviour
     [SerializeField] private Gradient gradient;
     public Light2D _light;
 
+    private void Start()
+    {
+        StartCoroutine(updateTime());
+    }
+
     void Update()
+    {
+
+    }
+
+    IEnumerator updateTime()
     {
         float percentage = Mathf.Clamp01(GameManager.instance.time / 1444);
         _light.color = gradient.Evaluate(percentage);
+
+        yield return new WaitForSeconds(2);
+
+        StartCoroutine(updateTime());
+
     }
 }
