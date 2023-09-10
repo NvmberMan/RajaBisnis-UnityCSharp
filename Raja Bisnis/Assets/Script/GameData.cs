5996c06fb8418f5cefb38b3d22552ae6869d5312
NumberMan <numberman3250@gmail.com>
2023-09-10 21:02:56 +0700

    hit api new

--- Raja Bisnis/Assets/Script/GameData.cs
+++ Raja Bisnis/Assets/Script/GameData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "GameData/GameData", order = 1)]
 public class GameData : ScriptableObject
 {
+    public int versionId;
+    public string versionName;
     public List<Data_Shop> Shop = new List<Data_Shop>();
 
 
@@ -29,16 +31,23 @@ public class Data_Menu
 {
     public string _name;
     public string _id;
+
     [TextAreaAttribute]
     public string _description;
-    public string _menu_display;
+    public Sprite _menu_sprite;
 
     [Header("Calculate")]
     public int _level_Max;
+
+    [Space(10)]
     public double _price;
     public double _price_multiplier;
     public double _price_unlock;
 
     [Space(10)]
-    public string _id_Shop;
+    public double _income;
+    public double _income_multiplier;
+
+    [Space(10)]
+    [HideInInspector]public string _id_Shop;
 }
--- Raja Bisnis/Assets/Script/LoadingGame.cs
+++ Raja Bisnis/Assets/Script/LoadingGame.cs
@@ -1,30 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class LoadingGame : MonoBehaviour
 {
-    public string URL = "localhost:3000";
-    public GameData gameData;
+    [SerializeField] private string URL = "localhost:3000";
+    [SerializeField] private string savePath = "Assets/Images/";
+
+    [SerializeField] private GameData gameData;
+    [SerializeField] private Slider loadingSlider;
+    [SerializeField] private TMP_Text loadingStatus;
+
+    int new_versionId;
+    string new_versionName;
     // Start is called before the first frame update
     void Start()
     {
+        loadingSlider.maxValue = 100;
         StartCoroutine(FetchData());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
     public IEnumerator FetchData()
     {
-        using (UnityWebRequest request = UnityWebRequest.Get(URL + "/gamedata"))
+        using (UnityWebRequest request = UnityWebRequest.Get(URL + "/gamedata/version/" + gameData.versionId))
         {
             yield return request.SendWebRequest();
             if (request.result == UnityWebRequest.Result.ConnectionError)
@@ -33,39 +36,167 @@ public class LoadingGame : MonoBehaviour
             }
             else
             {
-                string t = request.downloadHandler.text;
-                SimpleJSON.JSONNode json = SimpleJSON.JSON.Parse(t);
+                yield return new WaitForSeconds(0.5f);
+                loadingSlider.value = 20f;
+                loadingStatus.text = "Loading..";
+                Debug.Log("Checking Update..");
 
-                gameData.Shop.Clear();
 
-                foreach (SimpleJSON.JSONNode shopData in json["data"])
+
+                yield return new WaitForSeconds(1f);
+                loadingStatus.text = "Checking Update..";
+                loadingSlider.value = 35f;
+
+
+                if (CheckUpdate(request))
                 {
-                    List< Data_Menu> m = new List<Data_Menu>();
-                    foreach(SimpleJSON.JSONNode menuData in shopData["menu"])
-                    {
-                        m.Add(new Data_Menu() { 
-                            _id = menuData["id"],
-                            _name = menuData["description"],
-                            _level_Max = menuData["level_max"],
-                            _price = menuData["price"],
-                            _price_multiplier = menuData["price_multiplier"],
-                            _price_unlock = menuData["price_unlock"],
-                            _menu_display = menuData["menu_display"],
-                            _id_Shop = menuData["shopId"]
-                        });
-                    }
+                    Debug.Log("Updating Game..");
+                    string newURL = URL + "/gamedata";
 
-                    gameData.Shop.Add(new Data_Shop()
-                    {
-                        _id = shopData["IdShop"],
-                        _name = shopData["name"],
-                        _description = shopData["description"],
-                        _menu = m
-                    });
+
+                    yield return new WaitForSeconds(0.8f); // Tambahkan delay satu detik di sini
+                    StartCoroutine(FetchAndApplyUpdate(newURL));
                 }
+                else
+                {
+                    loadingStatus.text = "Loading Game...";
+                    loadingSlider.value = 80f;
 
 
+                    yield return new WaitForSeconds(0.5f);
+                    loadingStatus.text = "Starting Game";
+                    loadingSlider.value = 100f;
+
+                    //load game scene
+                    yield return new WaitForSeconds(0.2f);
+                    SceneManager.LoadScene("Game");
+                }
             }
+        }
+    }
+
+    private IEnumerator FetchAndApplyUpdate(string newURL)
+    {
+        using (UnityWebRequest updateRequest = UnityWebRequest.Get(newURL))
+        {
+            yield return updateRequest.SendWebRequest();
+            if (updateRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(updateRequest.error);
+            }
+            else
+            {
+                UpdateGame(updateRequest);
+                loadingStatus.text = "Loading Game...";
+                loadingSlider.value = 90f;
+
+
+                yield return new WaitForSeconds(0.5f);
+                loadingStatus.text = "Starting Game";
+                loadingSlider.value = 100f;
+
+                //load game scene
+                yield return new WaitForSeconds(0.2f);
+                //SceneManager.LoadScene("Game");
+            }
+        }
+    }
+
+
+
+    bool CheckUpdate(UnityWebRequest request)
+    {
+        string t = request.downloadHandler.text;
+        SimpleJSON.JSONNode json = SimpleJSON.JSON.Parse(t);
+
+        if (json["update"])
+        {
+            new_versionId = json["new_version"]["id"];
+            new_versionName = json["new_version"]["update_name"];
+        }
+        return json["update"];
+    }
+
+    void UpdateGame(UnityWebRequest request)
+    {
+        string t = request.downloadHandler.text;
+        SimpleJSON.JSONNode json = SimpleJSON.JSON.Parse(t);
+
+        gameData.Shop.Clear();
+
+        foreach (SimpleJSON.JSONNode shopData in json["data"])
+        {
+            List<Data_Menu> m = new List<Data_Menu>();
+            foreach (SimpleJSON.JSONNode menuData in shopData["menu"])
+            {
+                string menu_display_url = URL + "/menu/image/" + menuData["menu_display"];
+                string menu_display_name = menuData["id"];
+                StartCoroutine(DownloadAssets(menu_display_url, menu_display_name));
+
+                Sprite sprite = Resources.Load<Sprite>("Menu/" + menu_display_name);
+                m.Add(new Data_Menu()
+                {
+                    _id = menuData["id"],
+                    _name = menuData["name"],
+                    _description = menuData["description"],
+                    _level_Max = menuData["level_max"],
+                    _menu_sprite = sprite,
+
+                    _price = menuData["price"],
+                    _price_multiplier = menuData["price_multiplier"],
+                    _price_unlock = menuData["price_unlock"],
+
+                    _income = menuData["income"],
+                    _income_multiplier = menuData["income_multiplier"],
+
+                    _id_Shop = menuData["shopId"]
+                });
+            }
+
+            gameData.Shop.Add(new Data_Shop()
+            {
+                _id = shopData["IdShop"],
+                _name = shopData["name"],
+                _description = shopData["description"],
+                _menu = m
+            });
+        }
+        //gameData.versionId = new_versionId;
+        gameData.versionName = new_versionName;
+        Debug.Log("Successful update game");
+    }
+
+
+    //
+
+
+    IEnumerator DownloadAssets(string imageUrl, string imageName)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageUrl);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            // Mendapatkan teksur gambar
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+
+            // Membuat path lengkap untuk menyimpan gambar
+            string fullPath = savePath + imageName + ".png"; // Anda bisa menyimpannya dalam format yang sesuai
+
+            // Menyimpan teksur gambar ke dalam folder Assets
+            System.IO.File.WriteAllBytes(fullPath, texture.EncodeToPNG()); // Menggunakan PNG sebagai contoh
+
+            // Refresh aset di Unity Editor
+            UnityEditor.AssetDatabase.Refresh();
+
+            Debug.Log("Gambar berhasil diunduh dan disimpan di: " + fullPath);
+
+            
+        }
+        else
+        {
+            Debug.LogError("Gagal mengunduh gambar: " + www.error);
         }
 
     }
